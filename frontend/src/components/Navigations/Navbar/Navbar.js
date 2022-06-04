69f0bbeed73ece33ca5b8d8e4a6a6139beec026d
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-06-04 23:28:45 +0530

    added mobile view navigation

--- frontend/src/components/Navigations/Navbar/Navbar.js
+++ frontend/src/components/Navigations/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { DevHubLogo, DevHubLogoLight } from "../../../constants/Images";
 import "./Navbar.css";
@@ -20,7 +20,100 @@ const Navbar = ({ theme, children, show }) => {
           </div>
         </Link>
 
+        <NavbarSlider />
         <div className="header__children">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+const NavbarSlider = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="slider__main__container">
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <div
+          style={{
+            transform: open ? "translateY(7px) rotate(45deg) " : "rotate(0deg)",
+          }}
+          className="line"
+        />
+        <div
+          style={{
+            opacity: open ? "0" : "1",
+          }}
+          className="line"
+        />
+        <div
+          style={{
+            transform: open
+              ? " translateY(-7px) rotate(-45deg)"
+              : "rotate(0deg)",
+          }}
+          className="line"
+        />
+      </div>
+
+      <div className={`${open ? "slider active" : "slider"}`}>
+        <ul onClick={() => setOpen(false)} className="slider__list">
+          <li className="nav__logo__container">
+            <Link to="/">
+              <a
+                className="logo"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+                href="/"
+              >
+                <div>
+                  <img
+                    src={DevHubLogo}
+                    alt="devHub-logo"
+                    style={{
+                      width: "60px",
+                    }}
+                  />
+                </div>
+                <h3
+                  style={{
+                    fontSize: "1.6rem",
+                    color: "#008bb7",
+                  }}
+                >
+                  DevHub
+                </h3>
+              </a>
+            </Link>
+          </li>
+          <div
+            style={{
+              minWidth: "100%",
+            }}
+          >
+            <Link to="/">
+              <li className="menuItem">
+                <p>Home</p>
+              </li>
+            </Link>
+            <Link to="/about">
+              <li className="menuItem">
+                <p>about</p>
+              </li>
+            </Link>
+            <Link to="/pricing">
+              <li className="menuItem">
+                <p>Pricing</p>
+              </li>
+            </Link>
+            <Link to="/contact">
+              <li className="menuItem">
+                <p>Contact</p>
+              </li>
+            </Link>
+          </div>
+        </ul>
       </div>
     </div>
   );
