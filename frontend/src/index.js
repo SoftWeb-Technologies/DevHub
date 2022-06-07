fc7cd422e5d84f0b73a7bb69a859a55074e6877e
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-06-07 16:17:56 +0530

    updated ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { CookiesProvider } from "react-cookie";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
