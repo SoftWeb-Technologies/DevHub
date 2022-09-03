544f7b097874a2297346bd38702d53571b1b1943
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-09-04 00:10:31 +0530

    changed task display model

---
+++ frontend/src/DevHubIcons/ClockIcon.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ClockIcon = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.6668 7.99967C14.6668 11.6797 11.6802 14.6663 8.00016 14.6663C4.32016 14.6663 1.3335 11.6797 1.3335 7.99967C1.3335 4.31967 4.32016 1.33301 8.00016 1.33301C11.6802 1.33301 14.6668 4.31967 14.6668 7.99967Z"
+        stroke="#808080"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.4734 10.1202L8.40675 8.88684C8.04675 8.6735 7.75342 8.16017 7.75342 7.74017V5.00684"
+        stroke="#808080"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ClockIcon;
