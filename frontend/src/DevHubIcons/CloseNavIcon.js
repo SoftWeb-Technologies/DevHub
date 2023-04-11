6e527315e8291b8b64beb5ef1f880aa50175c435
Darshancodes <70362500+Darshancodes@users.noreply.github.com>
2023-04-11 11:44:56 +0000

    add: homepage color/image

--- frontend/src/DevHubIcons/CloseNavIcon.js
+++ frontend/src/DevHubIcons/CloseNavIcon.js
@@ -13,7 +13,7 @@ const CloseNavIcon = (props) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M5 4a1 1 0 00-1 1v14a1 1 0 001 1h3V4H5zm4-2H5a3 3 0 00-3 3v14a3 3 0 003 3h14a3 3 0 003-3V5a3 3 0 00-3-3H9zm1 2v16h9a1 1 0 001-1V5a1 1 0 00-1-1h-9zm6.707 10.707a1 1 0 000-1.414L15.414 12l1.293-1.293a1 1 0 00-1.414-1.414l-2 2a1 1 0 000 1.414l2 2a1 1 0 001.414 0z"
-      fill="Black"
+      fill="White"
     />
   </svg>
 );
