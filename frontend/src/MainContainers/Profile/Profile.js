e0abb9a4509a69a7aa6e318cd315c1da5ed24f81
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-04-23 14:56:44 +0530

    [updated]: profile page with new UI

--- frontend/src/MainContainers/Profile/Profile.js
+++ frontend/src/MainContainers/Profile/Profile.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Header } from "../components";
 import { TextInput, Title } from "../../components";
-import {thinkingImg} from "../../constants/Images";
+import { thinkingImg } from "../../constants/Images";
 import { MenuIcon } from "../../DevHubIcons";
 import { DashboardSideNavigation } from "../components";
 import "./Profile.css";
@@ -17,15 +17,7 @@ const Profile = () => {
       <Title title="Profile" />
 
       <DashboardSideNavigation setIsNavActive={setIsNavActive} />
-      <ProfileHeader
-      isNavActive={isNavActive}
-      displayName={
-        currentUser?.displayName ||
-        currentUser?.user?.name ||
-        currentUser.email ||
-        "User"
-      }
-      />
+      <ProfileHeader isNavActive={isNavActive} currentUser={currentUser} />
 
       <div className="profile__main__container">
         <div className="">
@@ -36,32 +28,20 @@ const Profile = () => {
               width: "100%",
               objectFit: "contain",
               cursor: "pointer",
-              position: "absolute", 
+              position: "absolute",
               top: "5%",
               left: "5%",
             }}
           >
             <MenuIcon />
           </div>{" "}
-          <h2 style={{ textAlign: "center", padding: "2rem 0" }}>
-            Edit Profile
-          </h2>
           <div className="profile__content__container">
-            <div className="profile__avatar__container">
-              <h1>
-                {currentUser?.displayName?.charAt(0) ||
-                  currentUser?.user?.name?.charAt(0) ||
-                  currentUser?.email?.charAt(0) ||
-                  "User"?.charAt(0)}
-              </h1>
-            </div>
-            <h2 style={{ color: "gray", marginBottom: "1rem", marginRight: "5%" }}>
+            <h2 style={{ color: "gray" }}>
               {currentUser?.displayName ||
                 currentUser?.user?.name ||
                 currentUser?.email?.split("@")[0] ||
                 "User"}
             </h2>
-
             <div className="profile__form__container">
               <TextInput
                 placeholder={"Name"}
@@ -79,16 +59,18 @@ const Profile = () => {
                 value={currentUser?.email || currentUser?.user?.email}
               />
             </div>
-            <div className="user-profile-inner"/>
+            {/* <div className="user-profile-inner" />
             <div className="user-profile-inner1">
-             <div className="image_wrapper">
-              <img className="thinking_image" alt="" src={thinkingImg} />
-             </div>
-             </div>
+              <div className="image_wrapper">
+                <img className="thinking_image" alt="" src={thinkingImg} />
+              </div>
+            </div>
             <div className="Facts__container">Do you know?</div>
             <div className="Facts__wrapper">
-              <div className="coding__fact">Coding will soon be as important as "reading"!</div>
-            </div>
+              <div className="coding__fact">
+                Coding will soon be as important as "reading"!
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -96,44 +78,17 @@ const Profile = () => {
   );
 };
 
-export const ProfileHeader = ({ isNavActive, displayName }) => {
+export const ProfileHeader = ({ isNavActive, displayName, currentUser }) => {
   return (
     <div className={`profile__header ${isNavActive} ? "active" : ""`}>
-      <div className="profile__header__container">
-        <Header displayName={displayName} />
-      </div>
-      <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        marginTop: "1rem",
-      }}
-      >
-        <h2
-        style={{
-          fontSize: "1.2rem",
-          color: "#fff",
-        }}
-        >
-          Your Profile
-        </h2>
-        <h2
-        className="hide-on-mobile"
-        style={{
-          fontSize: "1.2rem",
-          color: "#fff",
-        }}
-        >
-          What's up? {" "}
-          <span
-          style={{
-            color: "#0E80D3",
-          }}
-          >
-            {displayName}
-          </span>
-        </h2>
+      <div className="profile__header__container"></div>
+      <div className="profile__avatar__container profile_center">
+        <h1>
+          {currentUser?.displayName?.charAt(0) ||
+            currentUser?.user?.name?.charAt(0) ||
+            currentUser?.email?.charAt(0) ||
+            "User"?.charAt(0)}
+        </h1>
       </div>
     </div>
   );
